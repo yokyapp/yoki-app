6cb6b586d0decc6595793a1d8aed306b292acf17
vlakk <lawashkalash@gmail.com>
2024-06-20 23:42:11 +0300

    made normal scroll

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,28 +1,5 @@
 import './footer.scss'
 
 document.querySelector('#footer').innerHTML = `
-    <div class="links">
-        <a href="" class="soc-media">
-            <img src="/components/footer/telegram-icon-svg.svg" alt="">
-            
-            долучайся до нашого Telegram
-        </a>
-        <a href="" class="footer-logo">
-            <img src="/components/footer/yoki-logo-svg.svg" alt="">
-        
-            Натисніть на Yoky щоб підтримати наш проєкт.
-        </a>
-        <a href="" class="soc-media">
-            <img src="/components/footer/discord-icon-svg.svg" alt="">
-            долучайся до нашого Discord
-        </a>
-    </div>
-    <div class="big-name-logo-privacy">
-        <div class="name">
-<!--            <img src="/components/footer/yoky-name.svg" alt="">-->
-        </div>
-        <div class="privacy">
-            <p>Copyright 2024 Yoky. Всі права захищені.</p>
-        </div>
-    </div>
+    
 `
