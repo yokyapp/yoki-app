10f28f92d7f31fcedd15bff1afde8ee9c9d7771c
vlakk <lawashkalash@gmail.com>
2024-06-21 21:32:36 +0300

    fix github pages injurance

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: "/yoki-landing/"
+    base: "/yoki-landing/",
+    build: {
+        outDir: 'docs',
+    },
 })
+
+
+
+
+
